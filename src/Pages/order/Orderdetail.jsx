a35251ebce9f98e9d3b2a06f9ce44b238d31ca14
santosh <sk8065788@gmail.com>
2025-03-04 07:12:07 +0530

    orderdetail and viewdetail

--- src/Pages/order/Orderdetail.jsx
+++ src/Pages/order/Orderdetail.jsx
@@ -3,54 +3,104 @@ import React from 'react'
 function Orderdetail() {
   return (
    <>
-     <div className='flex flex-col mx-2 rounded-lg '>
-        <h3 className='font-semibold text-lg pl-3'>Order deail(#1111)</h3>
-        <div className='container w-full h-screen pl-2'>
-                <div className='w-full flex flex-row pt-2 gap-1'>
-                    <div className="w-9/12 bg-white ">
-                        <h3 className='text-base md:text-lg'>Order Information</h3>
-                        <div className='flex mt-1.5 w-full text-base'>
-                            <div className='w-4/12'>
-                                <p className='text-xs text-center'>Date</p>
-                                <p className='text-sm md:text-base text-center my-1.5 '>feb 19, 2023</p>
-                            </div>
-                            <div className='w-2/12'>
-                                <p className=' text-xs text-center '>Item</p>
-                                <p className='text-sm md:text-base text-center my-1.5'>02</p>
-                            </div>
-                            <div className='w-2/12'>
-                                <p className='text-sm md:text-base text-center '>Paid</p>
-                                <p className="px-1.5 py-1 text-green-500 bg-green-200 rounded-xl  flex justify-center text-sm md:text-base mx-4 my-1.5">Yes</p>
-                            </div>
-                            <div className='w-2/12'>
-                                <p className='text-xs text-center '>Status</p>
-                                <p className="px-1.5 py-1 text-green-500 bg-green-200 rounded-xl  flex justify-center text-sm md:text-base mx-1 my-1.5">Processed</p>
-                            </div>
-                           
-                            <div className='w-2/12'>
-                                <p className='text-xs text-center'>Total</p>
-                                <p className='text-sm md:text-base text-center my-1.5'>$2023</p>
-                            </div>
+   <h3 className='text-lg font-semibold px-10 mb-3 mt-2'>Order</h3>
+     <div className=' px-10 py-4 flex flex-row'>
+        
+        <div className='w-8/12 flex flex-col'>
+            <div className='w-full flex flex-col'>
+                    <div className='flex justify-between'>
+                        <p>Order Id (#$%%%)</p>
+                        <div className=' flex space-x-2.5 flex-row'>
+                            <button className='px-4 py-1 border border-blue-600'>Return</button>
+                            <button className='px-4 py-1 border border-blue-600'>Edit Order</button>
                         </div>
-                        <div className='flex w-full'>
-
-                        </div>
-
                     </div>
-                    <div className='w-4/12 bg-white'>
-                    <h3 className='font-semibold text-base pl-2'>Customer</h3>
-                        <div className='flex flex-row pl-2 space-x-2'>
-                            <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" className='h-16 w-14'  alt="" />
-                            <div className='flex flex-col'>
-                                <p className='text-base' >Kate Lau</p>
-                                <p className='text-xs'>san@gmail.com</p>
+            </div>
+            <div className='w-full flex bg-white px-1.5 py-2 mt-2 flex-row'>
+                <div className='text-center w-5/12'>
+                    Product
+                </div>
+                <div className='text-center w-3/12'>
+                    Total Amount
+                </div>
+                <div className='text-center w-2/12'>
+                    Order Items
+                </div>
+                <div className='text-center w-2/12'>
+                    Status
+                </div>
+            </div>
+            <div className='w-full flex bg-white px-1.5 py-2  flex-row'>
+                <div className='w-5/12'>
+                    <div className='flex flex-row'>
+                        <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSqs2NjlL8zYgbGKyO_ozdZ5f3Ku0TMjY8Oh-yQi20TvWlxwucLCCve4OmFSHsDVn5DQA96R6Q2pdT3QOe_g2ZWmifFfadQ07QQbtC5CCF3Eq24OdbutxVj9_s" className='h-12' alt="" />
+                        <p className='my-auto px-3'>Nike Air Force 1'07 LV8</p>
 
-                            </div>
-                        </div>
                     </div>
                 </div>
-              
+                <div className='text-center  w-3/12'>
+                    $122.45
+                </div>
+                <div className='text-center w-2/12'>
+                    01
+                </div>
+                <div className='w-2/12 '>
+                    <p className='bg-orange-200 rounded-xl px-2 py-1 text-center'>Processing</p>
+                </div>
+            </div>
+            <div className='w-full flex bg-white px-1.5 py-2  flex-row'>
+                <div className='w-5/12'>
+                    <div className='flex flex-row'>
+                        <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSqs2NjlL8zYgbGKyO_ozdZ5f3Ku0TMjY8Oh-yQi20TvWlxwucLCCve4OmFSHsDVn5DQA96R6Q2pdT3QOe_g2ZWmifFfadQ07QQbtC5CCF3Eq24OdbutxVj9_s" className='h-12' alt="" />
+                        <p className='my-auto px-3'>Nike Air Force 1'07 LV8</p>
+
+                    </div>
+                </div>
+                <div className='text-center  w-3/12'>
+                    $122.45
+                </div>
+                <div className='text-center w-2/12'>
+                    01
+                </div>
+                <div className='w-2/12 '>
+                    <p className='bg-orange-200 rounded-xl px-2 py-1 text-center'>Processing</p>
+                </div>
+            </div>
         </div>
+        <div className='w-4/12  bg-white flex flex-col mx-1.5'>
+                <div className='px-6 py-1'>
+                <h2 className='pt-3 pb-2 font-semibold text-lg'>Order Summery</h2>
+                <div className='flex py-1 text-gray-500 justify-between'>
+                    <p>Sub Total :</p>
+                    <p>$336.56</p>
+                </div>
+                <hr />
+                <div className='flex py-1 text-gray-500 justify-between'>
+                    <p>Discount :</p>
+                    <p>+$ 50</p>
+                </div>
+                <hr />
+                <div className='flex py-1 text-gray-500 justify-between'>
+                    <p>Delivery Charge :</p>
+                    <p>$33.56</p>
+                </div>
+                <hr />
+                <div className='flex py-1 text-gray-500 justify-between'>
+                    <p>Estimate Tax(18%) :</p>
+                    <p>$12</p>
+                </div>
+                <hr />
+                <div className='font-semibold py-1 flex  justify-between'>
+                    <p>Sub Amount :</p>
+                    <p>$3360.56</p>
+                </div>
+                </div>
+
+        </div>
+        
+
+        
+
      </div>
    </>
   )
