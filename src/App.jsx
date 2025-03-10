c4ab3b4b8e7390c568ce536da076c432a7ec488c
Trupti Chandwani <210303105014@paruluniversity.ac.in>
2025-03-10 12:53:03 +0530

    login signup jwt token added by shubham  and settings page redesigned code by trupti

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,109 @@
-import './App.css'
-import React from 'react'
-import Vieworder from './Pages/order/Vieworder'
-import Orderdetail from './Pages/order/Orderdetail'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Dashboard from './Pages/dashboard/dashboard'
-import Orderlayout from './Components/Orderlayout'
-import DashboardLayout from './Components/DashboardLayout'
-import Productlayout from './Components/Productlayout'
-import Product from "./Pages/product/Product"
-import Paymentlayout from './Components/Paymentlayout'
-import Payment from './Pages/payment/payment'
-import Customerlayout from './Components/Customerlayout'
-import Customer from './Pages/customer/customer'
-import Settinglayout from './Components/Settinglayout'
-import Setting from './Pages/setting/Setting'
-import AddProduct from './Pages/product/AddProduct'
+import "./App.css";
+import React from "react";
+import Vieworder from "./Pages/order/Vieworder";
+import Orderdetail from "./Pages/order/Orderdetail";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./Pages/dashboard/dashboard";
+import Orderlayout from "./Components/Orderlayout";
+import DashboardLayout from "./Components/DashboardLayout";
+import Productlayout from "./Components/Productlayout";
+import Product from "./Pages/product/Product";
+import Paymentlayout from "./Components/Paymentlayout";
+import Payment from "./Pages/payment/payment";
+import Customerlayout from "./Components/Customerlayout";
+import Customer from "./Pages/customer/customer";
+import Settinglayout from "./Components/Settinglayout";
+import Setting from "./Pages/setting/Setting";
+import AddProduct from "./Pages/product/AddProduct";
+import UserRegistration from "./Components/UserRegistration";
+import Login from "../src/Pages/Login";
+
 function App() {
   const router = createBrowserRouter([
-        {
-          path:"/",
-          element:<DashboardLayout/>,
-          children:[
-            {
-              path:"/",
-              element:<Dashboard/>
-            }
-          ]
+    {
+      path: "/",
+      element: <DashboardLayout />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
+    {
+      path: "/signup",
+      element: <UserRegistration />,
+    },
 
+    {
+      path: "/login",
+      element: <Login />,
+    },
+
+    //order
+    {
+      path: "/order", // No leading slash
+      element: <Orderlayout />,
+      children: [
+        {
+          path: "/order", // Nested route without leading slash
+          element: <Vieworder />,
         },
-        //order
         {
-          path: "/order", // No leading slash
-          element: <Orderlayout/>,
-          children: [
-            {
-              path: "/order", // Nested route without leading slash
-              element: <Vieworder />,
-            },
-            {
-              path: "order_detail", // Nested route without leading slash
-              element: <Orderdetail />,
-            },
-          ],
+          path: "order_detail", // Nested route without leading slash
+          element: <Orderdetail />,
         },
-        //product
+      ],
+    },
+    //product
+    {
+      path: "/product",
+      element: <Productlayout />,
+      children: [
         {
-          path:"/product",
-          element:<Productlayout/>,
-          children:[
-           { 
-            path:"/product",
-            element:<Product/>
-            }, { 
-              path:"add_product",
-              element:<AddProduct/>
-              }
-          ]
+          path: "/product",
+          element: <Product />,
         },
-        //payment
         {
-          path:"/payment",
-          element:<Paymentlayout/>,
-          children:[
-            { 
-              path:"/payment",
-              element:<Payment/>
-              }
-
-          ]
-
+          path: "add_product",
+          element: <AddProduct />,
         },
-        //customer
+      ],
+    },
+    //payment
+    {
+      path: "/payment",
+      element: <Paymentlayout />,
+      children: [
         {
-          path:"/customer",
-          element:<Customerlayout/>,
-          children:[
-            { 
-              path:"/customer",
-              element:<Customer/>
-            }
-
-          ]
-
+          path: "/payment",
+          element: <Payment />,
         },
-        //setting
+      ],
+    },
+    //customer
+    {
+      path: "/customer",
+      element: <Customerlayout />,
+      children: [
         {
-          path:"/setting",
-          element:<Settinglayout/>,
-          children:[
-            { 
-              path:"/setting",
-              element:<Setting/>
-            }
+          path: "/customer",
+          element: <Customer />,
+        },
+      ],
+    },
+    //setting
+    {
+      path: "/setting",
+      element: <Settinglayout />,
+      children: [
+        {
+          path: "/setting",
+          element: <Setting />,
+        },
+      ],
+    },
+  ]);
 
-          ]
-
-        }
-      
-  ])
-  
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
-
-export default App
+export default App;
